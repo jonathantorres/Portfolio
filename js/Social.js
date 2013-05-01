48f057f023bbaf8c9ffd69d3caac3088849f5544
Jonathan Torres <jonathantorres41@gmail.com>
2013-04-30 23:35:05 -0400

    Checkpoint, working on social feed.

--- js/Social.js
+++ js/Social.js
@@ -1,24 +1,39 @@
 (function(window) {
 
 	var $social,
-		$arrow,
-		$arrowPointer;
+		$openArrow,
+		$arrowPointer,
+		$socialClose,
+		$leftArrow,
+		$rightArrow,
+		$socialFeed;
 
 	function Social() {}
 
 	Social.prototype.init = function() {
 		cacheSelectors();
-		$arrow.on("click", arrowClicked);
+		$openArrow.on("click", openArrowClicked);
+		$socialClose.on("click", socialCloseClicked);
 	};
 
 	var cacheSelectors = function() {
-		$arrow = $("#open_arrow");
-		$arrowPointer = $arrow.find("img");
+		$social = $("#social");
+		$openArrow = $("#open_arrow");
+		$arrowPointer = $openArrow.find("img");
+		$socialClose = $(".social_close");
+		$leftArrow = $(".left_arrow");
+		$rightArrow = $(".right_arrow");
+		$socialFeed = $(".social_feed");
 	};
 
-	var arrowClicked = function(e) {
+	var openArrowClicked = function(e) {
 		e.preventDefault();
-		console.log("Ja!");
+		TweenMax.to($social, 0.7, { left : "0%", ease : Expo.easeOut } );
+	};
+
+	var socialCloseClicked = function(e) {
+		e.preventDefault();
+		TweenMax.to($social, 0.7, { left : "100%", ease : Expo.easeOut } );
 	};
 
 	window.Social = Social;
