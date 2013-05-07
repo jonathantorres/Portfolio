c836ad8463593015b1266de670672713c72ac01e
Jonathan Torres <jonathantorres41@gmail.com>
2013-05-06 23:52:39 -0400

    Contact form CSS and markup

--- contact.php
+++ contact.php
@@ -1,4 +1,10 @@
 <section id="contact">
-    <h1 class="title">Contact <span class="johnred">Me</span></h1>
-    <p>My contact info goes here :)</p>
+    <h1 class="title">Contact Me</h1>
+    <p>Contact me here dude!! :P</p>
+    <form action="#" id="contact_form">
+    	<input type="text" id="username" name="username" placeholder="NAME">
+    	<input type="email" id="email" name="email" placeholder="E-MAIL">
+    	<textarea id="message" name="message" placeholder="MESSAGE"></textarea>
+    	<input type="submit" id="send" name="send" value="SEND">
+    </form>
 </section>
