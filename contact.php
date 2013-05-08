ec21dfb9fb786638058b89f80d163f19eb52e792
Jonathan Torres <jonathantorres41@gmail.com>
2013-05-07 23:46:04 -0400

    Contact form validation.

--- contact.php
+++ contact.php
@@ -1,10 +1,14 @@
 <section id="contact">
     <h1 class="title">Contact Me</h1>
-    <p>Contact me here dude!! :P</p>
+    <p class="summary">Contact me here dude!! :P</p>
     <form action="#" id="contact_form">
     	<input type="text" id="username" name="username" placeholder="NAME">
-    	<input type="email" id="email" name="email" placeholder="E-MAIL">
+    	<input type="text" id="email" name="email" placeholder="E-MAIL">
     	<textarea id="message" name="message" placeholder="MESSAGE"></textarea>
-    	<input type="submit" id="send" name="send" value="SEND">
+    	<input type="submit" class="button" id="send" name="send" value="SEND">
     </form>
+    <div id="success">
+    	<p>Thanks for your message!</p>
+    	<a href="#" id="ok" class="button">OK</a>
+    </div>
 </section>
--- js/Contact.js
+++ js/Contact.js
@@ -1,14 +1,66 @@
 (function(window) {
 
-	var $contact;
+	var $contact,
+		$title,
+		$summary,
+		$contactForm,
+		$success;
 
-	function Contact() {
-		
-	}
+	function Contact() { }
 
 	Contact.prototype.init = function() {
-		$contact = $("#contact");
+		cacheSelectors();
 		$contact.addClass("viewedSection");
+
+		/**
+		 * Validate/Submit Form
+		 */
+		$contactForm.validate({
+			rules : {
+				username: "required",
+				email: { required : true, email : true },
+				message: "required"
+			},
+
+			messages : {
+				username: "",
+				email: "",
+				message: ""
+			},
+
+			submitHandler : function(form) {
+				var username = $("#username").val(),
+					useremail = $("#email").val(),
+					usermessage = $("#message").val();
+
+				console.log(form);
+
+				$.ajax({
+					type : "POST",
+					url : "php/submit_contact_form.php",
+					data : { un : username, ue : useremail, um : usermessage }, 
+					success : function() {				
+						$contactForm.fadeOut("normal", function() {
+							$success.fadeIn();
+						});
+					},
+					error : function() {
+						console.log("error!");
+					}
+				});
+			}
+		});
+	};
+
+	/**
+	 * Selectors
+	 */
+	var cacheSelectors = function() {
+		$contact = $("#contact");
+		$title = $(".title");
+		$summary = $(".summary");
+		$contactForm = $("#contact_form");
+		$success = $(".success");
 	};
 
 	window.Contact = Contact;
