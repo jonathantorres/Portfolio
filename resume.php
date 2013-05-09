d06cd2c08f52580966391f7f954b61ad4d9dab2f
Jonathan Torres <jonathantorres41@gmail.com>
2013-05-08 23:16:37 -0400

    CSS and markup for Resume page.

--- resume.php
+++ resume.php
@@ -1,4 +1,16 @@
 <section id="resume">
-    <h1 class="title">My <span class="johnred">Resume</span></h1>
-    <p>My resume info goes here :)</p>
-</section>
+    <div id="overview">
+    	<div class="circle">
+			<div class="circle inside">
+				<img src="img/thats_me.jpg" alt="me">
+			</div>
+		</div><!-- .circle -->
+
+		<h1 class="title">Resume</h1>
+		<p>Obtain a position in the interactive world to expand all my skills, create excellent work and grow professionally in the industry.</p>
+		<p>2 years of experience on Flash & ActionScript 3.0. Web Design & Development.</p>
+    </div><!-- #overview -->
+
+    <a href="#" class="social_arrow up_arrow"><img src="img/social_up_arrow.png"></a>
+    <a href="#" class="social_arrow down_arrow"><img src="img/social_down_arrow.png"></a>
+</section><!-- #resume -->
