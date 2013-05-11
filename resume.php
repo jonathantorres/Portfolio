b5570fc06a88c0b428fb16b8eddaba0db57ad18c
Jonathan Torres <jonathantorres41@gmail.com>
2013-05-10 23:18:25 -0400

    Working on the "Specialties" section of the resume.

--- resume.php
+++ resume.php
@@ -11,6 +11,22 @@
 		<p>2 years of experience on Flash & ActionScript 3.0. Web Design & Development.</p>
     </div><!-- #overview -->
 
+    <div id="specialties">
+    	<h1 class="title">Specialties</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+		<div id="skills_container">
+			<div class="circle">
+				<div class="subcircle">
+					<div class="progress_circle">
+						<div class="front_circle">
+							<p class="skill">HTML5</p>
+						</div>
+					</div>
+				</div>
+			</div><!-- .circle -->
+		</div>
+    </div>
+
     <a href="#" class="social_arrow up_arrow"><img src="img/social_up_arrow.png"></a>
     <a href="#" class="social_arrow down_arrow"><img src="img/social_down_arrow.png"></a>
 </section><!-- #resume -->
