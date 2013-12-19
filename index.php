9bd5b9dc6221b34d7f101944010704e3b204baa3
Jonathan Torres <jonathantorres41@gmail.com>
2013-12-18 22:40:50 -0400

    Some minor changes on splash page.

--- index.php
+++ index.php
@@ -8,16 +8,15 @@
 						</div>
 					</div>
 				    <h3>Hi, my name is <span class="johnsquared">Jonathan Torres</span></h3>
-				    <h3>I’m a Flash Developer <span class="johnsquared">@Nobox</span></h3>
+				    <h3>I’m a Developer <span class="johnsquared">@Nobox</span></h3>
 				    <h3>My Portfolio is under <span class="johnsquared">construction</span></h3>
 				</div><!-- .myinfo -->
-				
+
 				<div class="reachme">
 					<h4>Reach me by email <a href="mailto:info@jonathantorres.com" class="johnsquared">info@jonathantorres.com</a></h4>
 					<h4 class="find_me">
 						You can also find me at
 						<ul class="reach_icons clearfix">
-							<li><a class="fb" href="https://www.facebook.com/jonathantorres41" target="_blank"></a></li>
 							<li><a class="twitter" href="https://twitter.com/torres_jonathan" target="_blank"></a></li>
 							<li><a class="in" href="http://pr.linkedin.com/in/torresjonathan" target="_blank"></a></li>
 							<li><a class="github" href="https://github.com/jonathantorres" target="_blank"></a></li>
