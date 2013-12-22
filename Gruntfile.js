57a1d6244300f3734978b0ef3c380726f90f5e74
Jonathan Torres <jonathantorres41@gmail.com>
2013-12-22 14:24:18 -0400

    Grunt tasks restructure.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,137 +1,150 @@
-/*global module:false*/
+/* global module:false */
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-    // Task configuration.
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      },
-      dist: {
-        src: ['js/*.js'],
-        dest: 'js/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'js/<%= pkg.name %>.min.js'
-      }
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          // jQuery
-          $: true,
-          jQuery: true,
-          console: true,
+    // Project configuration.
+    grunt.initConfig({
 
-          // greensock
-          TweenMax: true,
-          TimelineMax: true,
-          Expo: true,
-          Power1: true,
-          Sine: true,
-          Raphael: true,
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
 
-          // site
-          Utils: true,
-          Social: true,
-          Contact: true,
-          Welcome: true,
-          Portfolio: true,
-          Resume: true,
-          Site: true,
-          Preloader: true,
-          hasher: true
-        }
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
-      },
-      debug: {
-        src: 'js/*.js'
-      }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    sass: {
-      dev: {
-        options: {
-          style: 'expanded'
+        banner: [
+            '/**',
+            ' * <%= pkg.description %>',
+            ' * Version: <%= pkg.version %>',
+            ' * Author: <%= pkg.author.name %> | <%= pkg.author.email %>',
+            ' * Compiled on <%= grunt.template.today("yyyy-mm-dd") %>',
+            ' */\n\n'
+        ].join('\n'),
+
+        // Concatenate files.
+        concat: {
+            options: {
+                banner: '<%= banner %>',
+                stripBanners: true
+            },
+
+            dist: {
+                src: ['src/<%= pkg.name %>/*.js'],
+                dest: 'js/<%= pkg.name %>.js'
+            }
         },
-        files: {
-          'css/main.css' : 'sass/main.scss'
-        }
-      },
-      deploy: {
-        options: {
-          style: 'compressed'
+
+        // Minify files.
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+
+            dist: {
+                src: '<%= concat.dist.dest %>',
+                dest: 'js/<%= pkg.name %>.min.js'
+            }
         },
-        files: {
-          'css/main.css' : 'sass/main.scss'
+
+        // JS Hint options.
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                unused: true,
+                boss: true,
+                eqnull: true,
+                browser: true,
+                globals: {
+                  // jQuery
+                  $: true,
+                  jQuery: true,
+                  console: true,
+
+                  // greensock
+                  TweenMax: true,
+                  TimelineMax: true,
+                  Expo: true,
+                  Power1: true,
+                  Sine: true,
+                  Raphael: true,
+
+                  // site
+                  Utils: true,
+                  Social: true,
+                  Contact: true,
+                  Welcome: true,
+                  Portfolio: true,
+                  Resume: true,
+                  Site: true,
+                  Preloader: true,
+                  hasher: true
+                }
+            },
+
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+
+            debug: {
+                src: '<%= concat.dist.src %>'
+            }
+        },
+
+        // SASS settings.
+        sass: {
+            dev: {
+                options: {
+                    style: 'expanded'
+                },
+
+                files: {
+                    'css/main.css' : 'sass/main.scss'
+                }
+            },
+
+            deploy: {
+                options: {
+                    style: 'compressed'
+                },
+
+                files: {
+                    'css/main.css' : 'sass/main.scss'
+                }
+            }
+        },
+
+        // Grunt watch settings.
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+
+            sass: {
+                files: 'sass/main.scss',
+                tasks: 'sass:dev'
+            },
+
+            jshint: {
+                files: '<%= jshint.debug.src %>',
+                tasks: ['jshint:debug', 'concat']
+            }
         }
-      }
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'qunit']
-      },
-      sass: {
-        files: 'sass/main.scss',
-        tasks: 'sass:dev'
-      },
-      jshint: {
-        files: 'js/*.js',
-        tasks: 'jshint:debug'
-      }
-    }
-  });
+    });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'sass:dev']);
+    // Default task.
+    grunt.registerTask('default', ['jshint', 'sass:dev', 'concat']);
 
-  // Deployment task
-  grunt.registerTask('deploy', ['jshint', 'sass:deploy', 'concat', 'uglify']);
+    // Deployment task
+    grunt.registerTask('deploy', ['jshint', 'sass:deploy', 'concat', 'uglify']);
 
 };
